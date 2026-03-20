0a946da9516314436a5512b170fcb852edac25aa
Viacheslav Ch. <vchychyrko@anaconda.com>
2026-03-20 18:29:50 +0200

    fixed broken ABI

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,6 @@
 :: cmd
 echo "Building %PKG_NAME%."
 
-
 :: Isolate the build.
 mkdir Build-%PKG_NAME%
 cd Build-%PKG_NAME%
@@ -14,20 +13,29 @@ cmake .. %CMAKE_ARGS% ^
       -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
       -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -DCMAKE_BUILD_TYPE=Release ^
-      -DENABLE_GD_FORMATS=0 ^
+      -DCMAKE_C_FLAGS="/Dssize_t=SSIZE_T /DSSIZE_MAX=MAXSSIZE_T" ^
+      -DWEBP_LIBRARY=%PREFIX%\Library\lib\libwebp.lib ^
+      -DWEBP_INCLUDE_DIR=%PREFIX%\Library\include ^
+      -DBUILD_SHARED_LIBS=ON ^
+      -DBUILD_STATIC_LIBS=OFF ^
       -DENABLE_PNG=1 ^
       -DENABLE_JPEG=1 ^
       -DENABLE_TIFF=1 ^
       -DENABLE_FREETYPE=1 ^
-      -DENABLE_FONTCONFIG=1 ^
       -DENABLE_WEBP=1 ^
       -DENABLE_GD_FORMATS=1 ^
-      -DBUILD_TEST=0 ^
-      -DWEBP_LIBRARY=%PREFIX%/Library/lib/libwebp.lib ^
-      -DWEBP_INCLUDE_DIR=%PREFIX%/Library/include     ^
-      -DCMAKE_C_FLAGS="%CFLAGS% -Dssize_t=\"__int64\"" ^
-      -DCMAKE_CXX_FLAGS="%CXXFLAGS% -Dssize_t=\"__int64\"" ^
-      -DVERBOSE_MAKEFILE=1
+      -DENABLE_FONTCONFIG=1 ^
+      -DVERBOSE_MAKEFILE=1 ^
+      -DENABLE_LIQ=0 ^
+      -DENABLE_XPM=0 ^
+      -DENABLE_AVIF=0 ^
+      -DENABLE_HEIF=0 ^
+      -DENABLE_RAQM=0 ^
+      -DENABLE_ASAN=0 ^
+      -DENABLE_ICONV=0 ^
+      -DBUILD_TEST=0
+
+if errorlevel 1 exit /b 1
 
 :: Build.
 echo "Building..."
@@ -37,19 +45,16 @@ if errorlevel 1 (
   exit /b 1
 )
 
-
 :: Perform tests.
 @REM build tests fails with add_subdirectory given source "gdmatrix" which is not an existing directory.
 @REM echo "Testing..."
 @REM ctest -VV --output-on-failure
 @REM if errorlevel 1 exit /b 1
 
-
 :: Install.
 echo "Installing..."
 ninja install
 if errorlevel 1 exit /b 1
-
 
 :: Error free exit.
 echo "Error free exit!"
